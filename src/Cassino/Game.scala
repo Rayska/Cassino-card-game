4b02601cac462c07ae9cded4bfbf25200e82a9b5
Ray <ray.sointula@aalto.fi>
2023-02-26 01:36:29 +0200

    Added val roundsPlayed to Game.scala to clarify who the dealer is. This will be utilized in Round.scala to keep record who's turn it is.
    Also added some parameters and return values for Round.scala methods

--- src/Cassino/Game.scala
+++ src/Cassino/Game.scala
@@ -7,9 +7,10 @@ class Game(val players: Vector[Player]){
 
   val scores:       Buffer[(Player, Int)] = players.zip(Vector.fill(players.size)(0)).toBuffer
   val endCondition: Boolean               = false
+  var roundsPlayed = 0
 
-  def playRound(players: Vector[Player]): Unit =
-    new Round(players)
+  def playRound(players: Vector[Player], roundsPlayed: Int): Unit =
+    new Round(players, roundsPlayed)
 
   def addScore(score: Int, player: Player): Unit =
     scores(player.playerNumber) = (player, score)
--- src/Cassino/Round.scala
+++ src/Cassino/Round.scala
@@ -1,24 +1,30 @@
 package Cassino
 import scala.collection.mutable.Buffer
 
-class Round(val players: Vector[Player]) {
+class Round(val players: Vector[Player], val dealer: Int) {
+
+  private var turn: Int           = (dealer + 1) % players.size // which players turn it is, starting from the player next to the dealer. Starts from 0
+
   val deck:         Deck          = new Deck
   val tableCards:   Buffer[Card]  = Buffer[Card]()
   var endCondition: Boolean       = false
 
 
-  def playTurn: Unit =
+
+  def playTurn: Vector[String] =
+    ???
+    turn += 1
+    Vector[String]()
+
+  def checkLegitimacy(move: Vector[String]): Boolean =
     ???
 
-  def checkLegitimacy: Boolean =
-    ???
-
-  def dealCards: Unit =
-    for player <- players do //for players
+  def dealCards(players: Vector[Player]): Unit =
+    for player <- players do //to the players
       val hand = Buffer.tabulate(4)(k => deck.draw)
       for card <- hand do
         player.addCardToPlayer(card)
-    for card <- Buffer.tabulate(4)(k => deck.draw) do
+    for card <- Buffer.tabulate(4)(k => deck.draw) do //to the table
       tableCards += card
 
 }
