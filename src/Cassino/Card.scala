a20f6a54572d4906a678ce395284e4fdbbd34962
Ray <ray.sointula@aalto.fi>
2023-02-26 12:11:43 +0200

    Card.scala : Added/modified values handValue and tableValue and changed the constructor parameter from handValue to CardID to better describe it's use. handValues and tableValues now get the correct values from their CardID

--- src/Cassino/Card.scala
+++ src/Cassino/Card.scala
@@ -1,5 +1,22 @@
 package Cassino
 
-class Card(val handValue: Int) {
-  val suit = handValue / 13 // 0 for Clubs, 1 for Diamonds, 2 for Hearts and 3 for Spades
+class Card(val cardID: Int) {
+  val suit: Int = cardID / 13       // 0 for Clubs, 1 for Diamonds, 2 for Hearts and 3 for Spades
+  val handValue: Int = {
+    if (cardID + 1) % 13 == 0 then  // Aces
+      14
+    else if cardID == 21 then       // Diamonds 10
+      16
+    else if cardID == 39 then       // Spades 3
+      15
+    else
+      cardID % 13 + 2
+  }
+  val tableValue: Int = {
+    if (cardID + 1) % 13 == 0 then  // Aces
+      1
+    else
+      cardID % 13 + 2
+  }
+
 }
