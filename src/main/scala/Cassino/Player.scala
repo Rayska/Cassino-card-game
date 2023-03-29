f279ab39525cafdafae90e2fe3ccde23c38ad4f7
Ray <ray.sointula@aalto.fi>
2023-03-30 00:09:30 +0300

    Change from IDEA to sbt for easy access to scalaFX

---
+++ src/main/scala/Cassino/Player.scala
@@ -0,0 +1,29 @@
+package Cassino
+
+import scala.collection.mutable.Buffer
+
+abstract class Player {
+
+  val cards:        Buffer[Card]
+  val pile:         Buffer[Card]
+  val playerNumber: Int
+  val playerName:   String
+
+  def returnRoundScore: Int
+  
+  def clearHand: Unit
+
+  def addCardToPile(card: Card):   Unit
+
+  def addCardToPlayer(card: Card): Unit
+
+  def addPoints(add: Int): Unit
+
+  def returnPileSize: Int
+
+  def returnSpadesSize: Int
+
+  def changeGame(game: Game): Unit
+
+  def returnGame: Game
+}
